85cff3f28a47ca6a73df8b86f9576264233615a8
mrpedrobraga <mrhenribraga@gmail.com>
2023-05-30 08:22:30 -0300

    renamed shit

--- Control.gd
+++ Control.gd
@@ -1,32 +1,35 @@
 extends Control
+class_name SlerpTweener2
 
-export var vector1 : Vector2 = Vector2.LEFT
-export var vector2 : Vector2 = Vector2.RIGHT
-export var old_vector1 : Vector2 = Vector2.LEFT
-export var interpolation_progress = 0
-export var interpolation_duration = 0.2 # In seconds
+export var vector : Vector2 = Vector2.LEFT
+export var target_vector : Vector2 = Vector2.RIGHT
+export var old_vector : Vector2 = Vector2.LEFT
+export var progress = 0
+export var duration_seconds = 0.2
 
 func _physics_process(delta):
-	interpolation_progress += delta / interpolation_duration
-	if interpolation_progress > 0:
-		vector1 = old_vector1.slerp(vector2, interpolation_progress)
+	progress += delta / duration_seconds
+	if progress > 0:
+		vector = old_vector.slerp(target_vector, progress)
 		
-		if (is_equal_approx(interpolation_progress, 1) or interpolation_progress >= 1):
-			vector1 = vector2
-			old_vector1 = vector1
-			interpolation_progress = -1
+		if (is_equal_approx(progress, 1) or progress >= 1):
+			vector = target_vector
+			old_vector = vector
+			progress = -1
 			update()
 	update()
 
-func interpolate_vector2():
-	vector1 = old_vector1
-	interpolation_progress = 0
+func interpolate(initial_value, final_value):
+	if initial_value:
+		vector = initial_value
+	old_vector = vector
+	target_vector = final_value
+	progress = 0
 
 func _input(ev):
 	if ev is InputEventMouseButton:
 		if ev.pressed:
-			vector2 = (ev.position - Vector2(64,64)/2).normalized()
-			interpolate_vector2()
+			interpolate(null, (ev.position - Vector2(64,64)/2).normalized())
 
 func _draw():
 	draw_set_transform(
@@ -35,6 +38,6 @@ func _draw():
 		Vector2(8,8)
 	)
 	
-	draw_line(Vector2(0, 0), vector1 * 2, Color.red, 1)
-	#draw_line(Vector2(0, 0), old_vector1, Color.white, 1.0)
-	#draw_line(Vector2(0, 0), vector2, Color.blue, 1.0)
+	draw_line(Vector2(0, 0), vector * 2, Color.red, 1)
+	#draw_line(Vector2(0, 0), old_vector, Color.white, 1.0)
+	#draw_line(Vector2(0, 0), target_vector, Color.blue, 1.0)
